90b732aa93079309e7c43be69c62d22fc9bee8da
Andras E <rohadtsajt@gmail.com>
2022-11-15 20:20:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "https://andras-secrets.cyclic.app/auth/facebook/secrets"
+    callbackURL: "https://sore-ruby-caterpillar-wrap.cyclic.app/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({
